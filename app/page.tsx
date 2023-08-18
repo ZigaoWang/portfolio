6e3f4370154480381b2332a4c71ee4ad694375fe
Zigao Wang <zigao.wang24@gmail.com>
2023-08-19 01:43:31 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -28,14 +28,22 @@ export default function Home() {
 				className="absolute inset-0 -z-10 animate-fade-in"
 				quantity={100}
 			/>
-			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-5xl md:text-9xl whitespace-nowrap bg-clip-text ">
 				Zigao Wang
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hello, my name is Zigao Wang, I'm I am a passionate front-end developer with expertise in HTML, CSS, and JavaScript. I'm now working on the project {" "}
+					Hello, my name is Zigao Wang.
+					
+					<br />
+					
+					I'm I am a passionate front-end developer with expertise in HTML, CSS, and JavaScript.
+					
+					<br />
+					
+					I'm now working on the project {" "}
 					<Link
 						target="_blank"
 						href="https://lite.daztab.com"
