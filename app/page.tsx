283947ea259144f5c3816ed3016c4ba94399b1a9
Zigao Wang <zigao.wang24@gmail.com>
2023-08-19 01:25:32 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,26 +35,15 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Andreas, I'm building serverless and open source
-					solutions at{" "}
+					Hello, my name is Zigao Wang, I'm I am a passionate front-end developer with expertise in HTML, CSS, and JavaScript. I'm now working on the project {" "}
 					<Link
 						target="_blank"
-						href="https://upstash.com"
+						href="https://lite.daztab.com"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						Upstash
-					</Link>
-
-					<br />
-					and working on{" "}
-					<Link
-						target="_blank"
-						href="https://unkey.dev"
-						className="underline duration-500 hover:text-zinc-300"
-					>
-						unkey.dev
+						DazTab Lite
 					</Link>{" "}
-					at night.
+					.
 				</h2>
 			</div>
 		</div>
