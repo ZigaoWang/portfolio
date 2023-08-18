6adf35ebc23acacb9c78de7a0c6f5f1923cd74b8
Zigao Wang <zigao.wang24@gmail.com>
2023-08-19 02:39:14 +0800

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -46,7 +46,7 @@ export default async function ProjectsPage() {
 						Projects
 					</h2>
 					<p className="mt-4 text-zinc-400">
-						Some of the projects are from work and some are on my own time.
+						These are the projects I made during my own time.
 					</p>
 				</div>
 				<div className="w-full h-px bg-zinc-800" />
