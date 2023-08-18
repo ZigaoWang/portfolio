8bf6310e42731c7a654461b7b372208f196c3e11
Zigao Wang <zigao.wang24@gmail.com>
2023-08-19 02:27:11 +0800

    Update analytics.tsx

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -6,10 +6,6 @@ export function Analytics() {
 		return null;
 	}
 	return (
-		<script
-			src="https://beamanalytics.b-cdn.net/beam.min.js"
-			data-token=95124af6-91c5-4ccb-b8bb-625f2288649d
-			async
-		/>
+		<script src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="95124af6-91c5-4ccb-b8bb-625f2288649d" async></script>
 	);
 }
